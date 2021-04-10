9219b8f8194d68a1f1757f9f8c0283de7627831a
방규빈 <rbqls1057@gmail.com>
2021-04-10 16:46:51 +0900

    login and likes

--- src/main/java/com/test/controller/LikesController.java
+++ src/main/java/com/test/controller/LikesController.java
@@ -1,5 +1,6 @@
 package com.test.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.test.dto.LectureDto;
 import com.test.dto.UserDto;
 import com.test.service.likes.LikesService;
@@ -12,8 +13,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
 
-@Controller
+@RestController
 public class LikesController {
 
     @Autowired
@@ -21,10 +23,12 @@ public class LikesController {
 
     private final SimpleDateFormat dateForDB = new SimpleDateFormat("yyyy-MM-dd");
 
-    @PostMapping("/user/login/likes/insert")
-    public String insert(@RequestParam(value = "lecNo") String lecNo, // 찜 추가
-                         HttpServletRequest request) {
-        System.out.println(lecNo);
+    @RequestMapping("/user/likes/insert")
+    public boolean insert(@RequestBody Map<String, String> lecNo, // 찜 추가
+                          HttpServletRequest request) {
+        System.out.println(lecNo.isEmpty());
+        String no = lecNo.get("lecNo");
+        System.out.println(no);
 
         HttpSession session = request.getSession();
         UserDto userDto = (UserDto) session.getAttribute("userLogin");
@@ -32,24 +36,26 @@ public class LikesController {
         Date currentTime = new Date();
         String likeDateForDB = dateForDB.format(currentTime);
 
-        likesService.insertLecture(userDto.getUserNo(), lecNo, likeDateForDB);
-        System.out.println("Success insert likes " + userDto.getUserNo() + " " + lecNo);
+        boolean result = likesService.insertLecture(userDto.getUserNo(), no, likeDateForDB);
+        System.out.println("Success insert likes " + userDto.getUserNo() + " " + no);
 
-        return "redirect:/";
+        return result;
     }
 
-    @PostMapping("/user/login/likes/delete")
-    public String delete(@RequestParam(value = "lecNo") String lecNo, // 찜 해제
-                         HttpServletRequest request) {
-        System.out.println(lecNo);
+    @RequestMapping("/user/likes/delete")
+    public boolean delete(@RequestBody Map<String, String> lecNo, // 찜 해제
+                          HttpServletRequest request) {
+        System.out.println(lecNo.isEmpty());
+        String no = lecNo.get("lecNo");
+        System.out.println(no);
 
         HttpSession session = request.getSession();
         UserDto userDto = (UserDto) session.getAttribute("userLogin");
 
-        likesService.deleteLecture(userDto.getUserNo(), lecNo);
-        System.out.println("Success delete likes " + userDto.getUserNo() + " " + lecNo);
+        boolean result = likesService.deleteLecture(userDto.getUserNo(), no);
+        System.out.println("Success delete likes " + userDto.getUserNo() + " " + no);
 
-        return "redirect:/";
+        return result;
     }
 
 }
